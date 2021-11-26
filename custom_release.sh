14851f02a1dbab390c4943e6032ff1a4cb0f9e80
Karel Hala <khala@redhat.com>
2021-11-26 15:02:02 +0100

    Fix path to travis release script

--- custom_release.sh
+++ custom_release.sh
@@ -8,7 +8,7 @@ then
     do
         echo "PUSHING ${env}-beta"
         rm -rf ./dist/.git
-        .travis/release.sh "${env}-beta"
+        ./scripts/release.sh "${env}-beta"
     done
 fi
 
@@ -18,12 +18,12 @@ then
     do
         echo "PUSHING ${env}-stable"
         rm -rf ./dist/.git
-        .travis/release.sh "${env}-stable"
+        ./scripts/release.sh "${env}-stable"
     done
 fi
 
 if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
     echo "PUSHING ${TRAVIS_BRANCH}"
     rm -rf ./build/.git
-    .travis/release.sh "${TRAVIS_BRANCH}"
+    ./scripts/release.sh "${TRAVIS_BRANCH}"
 fi
